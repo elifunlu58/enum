1d4a621afc3bcf078006b85f419b38830b66e936
elif <48827713+elifunlu58@users.noreply.github.com>
2020-09-03 20:14:18 +0300

    Add files via upload

---
+++ enum.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	
+	const float vetki=0.4;
+	const float fetki=0.6;
+	int vize,fin;
+	float ort;
+	cout<<"vize notunuzu giriniz";
+	cin>>vize;
+	cout<<"final notunuzu giriniz";
+	cin>>fin;
+	ort=(vize*vetki)+(fin*fetki);
+	cout<<"ortalamaniz="<<ort;
+	return 0;
+	
+	
+	
+	
+	
+}
